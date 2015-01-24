c5c04e2a6e64c514a39f6895f955d156fb12cb1a
V <email at email dot email>
2015-01-23 22:32:27 -0800

    server start. get request only. config stub and /slicer/jobs endpoint in place

---
+++ cmd/snuggied/snuggied.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var config = map[string]string{
+	"version": "0.0.0",
+}
+
+func snuggiedHandler(config map[string]string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case (r.Method == "GET"):
+			fmt.Fprint(w, "a GET")
+
+		default:
+			fmt.Fprint(w, "GET only")
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/slicer/jobs/", snuggiedHandler(config))
+
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
