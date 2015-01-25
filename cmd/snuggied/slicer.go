42db3507ba5f739d027459a4f30bb63bbf493581
Bryan Matsuo <bryan.matsuo@gmail.com>
2015-01-25 01:21:09 -0800

    fill out logic for running slicers

--- cmd/snuggied/slicer.go
+++ cmd/snuggied/slicer.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"os/exec"
 )
 
 type SlicerCmd struct {
-	Bin  string
-	Args []string
-	Log  io.Writer
+	Bin    string
+	Args   []string
+	OutLog io.Writer
+	ErrLog io.Writer
 }
 
 type Slicer interface {
@@ -17,7 +20,31 @@ type Slicer interface {
 }
 
 func Run(s Slicer, kill <-chan struct{}) error {
-	return fmt.Errorf("x")
+	scmd := s.SlicerCmd()
+	cmd := exec.Command(scmd.Bin, scmd.Args...)
+	cmd.Stdout = scmd.OutLog
+	cmd.Stderr = scmd.ErrLog
+	err := cmd.Start()
+	if err != nil {
+		return fmt.Errorf("%s: %v", scmd.Bin, err)
+	}
+	done := make(chan struct{})
+	go func() {
+		select {
+		case <-done:
+		case <-kill:
+			err := cmd.Process.Kill()
+			if err != nil {
+				log.Printf("kill: %v", err)
+			}
+		}
+	}()
+	err = cmd.Wait()
+	close(done)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 type Slic3r struct {
@@ -46,8 +73,9 @@ func (s *Slic3r) SlicerCmd() *SlicerCmd {
 		args = append(args, in)
 	}
 	return &SlicerCmd{
-		Bin:  bin,
-		Args: args,
-		Log:  os.Stderr,
+		Bin:    bin,
+		Args:   args,
+		OutLog: os.Stderr,
+		ErrLog: os.Stderr,
 	}
 }
